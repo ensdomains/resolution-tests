37dae80ff827057cf307e17845c8ffd418a181e1
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2026-03-21 14:48:17 +0800

    Raffy resolver

--- contracts/src/URTestResolver.sol
+++ contracts/src/URTestResolver.sol
@@ -1,66 +1,78 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {ENS as IENS} from "@ensdomains/registry/ENS.sol";
-import {Multicallable} from "@ensdomains/resolvers/Multicallable.sol";
-import {AddrResolver} from "@ensdomains/resolvers/profiles/AddrResolver.sol";
-import {ContentHashResolver} from "@ensdomains/resolvers/profiles/ContentHashResolver.sol";
-import {TextResolver} from "@ensdomains/resolvers/profiles/TextResolver.sol";
-import {IUniversalResolver} from "@ensdomains/universalResolver/IUniversalResolver.sol";
+import {IExtendedResolver} from "@ens/resolvers/profiles/IExtendedResolver.sol";
+import {IAddressResolver} from "@ens/resolvers/profiles/IAddressResolver.sol";
+import {IAddrResolver} from "@ens/resolvers/profiles/IAddrResolver.sol";
+import {ITextResolver} from "@ens/resolvers/profiles/ITextResolver.sol";
+import {ENSIP19, COIN_TYPE_ETH} from "@ens/utils/ENSIP19.sol";
 
 interface IProxy {
     function implementation() external view returns (address);
 }
 
-/// @dev ENS resolver that only returns data when called via the Universal Resolver. Useful for integration testing.
-contract URTestResolver is Multicallable, AddrResolver, ContentHashResolver, TextResolver {
-    IENS public constant ENS = IENS(0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e);
-    address public constant UNIVERSAL_RESOLVER = 0xeEeEEEeE14D718C2B47D9923Deab1335E144EeEe;
+/// @dev ENS resolver that returns different values if it's called from the Universal Resolver for integration testing.
+contract URTestResolver is IExtendedResolver, IAddressResolver, IAddrResolver, ITextResolver {
+    error UnsupportedResolverProfile(bytes4);
 
-    function text(bytes32 node, string calldata key) external view override returns (string memory) {
-        if (msg.sender != _ur()) {
+    address public immutable UR;
+
+    constructor(address ur) {
+        UR = ur;
+    }
+
+    function supportsInterface(bytes4 interfaceId) external view returns (bool) {
+        return msg.sender == IProxy(UR).implementation()
+            ? interfaceId == type(IExtendedResolver).interfaceId
+            : (interfaceId == type(IAddressResolver).interfaceId || interfaceId == type(IAddrResolver).interfaceId
+                    || interfaceId == type(ITextResolver).interfaceId);
+    }
+
+    function addr(bytes32) external pure returns (address payable) {
+        return payable(_addr(false));
+    }
+
+    function addr(bytes32, uint256 coinType) external pure returns (bytes memory) {
+        return _addr(coinType, false);
+    }
+
+    function text(bytes32, string calldata key) external pure returns (string memory) {
+        return _text(key, false);
+    }
+
+    function resolve(bytes calldata, bytes calldata data) external pure returns (bytes memory) {
+        if (bytes4(data) == IAddrResolver.addr.selector) {
+            return abi.encode(_addr(true));
+        } else if (bytes4(data) == IAddressResolver.addr.selector) {
+            (, uint256 coinType) = abi.decode(data[4:], (bytes32, uint256));
+            return abi.encode(_addr(coinType, true));
+        } else if (bytes4(data) == ITextResolver.text.selector) {
+            (, string memory key) = abi.decode(data[4:], (bytes32, string));
+            return abi.encode(_text(key, true));
+        } else {
+            revert UnsupportedResolverProfile(bytes4(data));
+        }
+    }
+
+    function _text(string memory key, bool ok) internal pure returns (string memory) {
+        if (keccak256(bytes(key)) == keccak256(bytes("description"))) {
+            return ok ? unicode"✅️ Universal Resolver" : unicode"❌️ Universal Resolver";
+        } else {
             return "";
         }
-        return versionable_texts[recordVersions[node]][node][key];
     }
 
-    function contenthash(bytes32 node) external view override returns (bytes memory) {
-        if (msg.sender != _ur()) {
-            return new bytes(0);
+    function _addr(bool ok) internal pure returns (address) {
+        return address(bytes20(_addr(COIN_TYPE_ETH, ok)));
+    }
+
+    function _addr(uint256 coinType, bool ok) internal pure returns (bytes memory) {
+        if (ENSIP19.isEVMCoinType(coinType)) {
+            return abi.encodePacked(
+                ok ? 0x2222222222222222222222222222222222222222 : 0x1111111111111111111111111111111111111111
+            );
+        } else {
+            return "";
         }
-        return versionable_hashes[recordVersions[node]][node];
-    }
-
-    function addr(bytes32 node) public view override returns (address payable) {
-        if (msg.sender != _ur()) {
-            return payable(address(0));
-        }
-        return super.addr(node);
-    }
-
-    function addr(bytes32 node, uint256 coinType) public view override returns (bytes memory) {
-        if (msg.sender != _ur()) {
-            return new bytes(64);
-        }
-        return super.addr(node, coinType);
-    }
-
-    function supportsInterface(bytes4 interfaceID)
-        public
-        view
-        override(Multicallable, AddrResolver, ContentHashResolver, TextResolver)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceID);
-    }
-
-    /// @dev Does not support wrapped names.
-    function isAuthorised(bytes32 node) internal view override returns (bool) {
-        address owner = ENS.owner(node);
-        return owner == msg.sender;
-    }
-
-    function _ur() internal view returns (address) {
-        return IProxy(UNIVERSAL_RESOLVER).implementation();
     }
 }
